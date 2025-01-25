539d1a246a72b11f4cf6e129fde4b4a521848128
ctclg <thomas.lundborg@gmail.com>
2025-01-26 00:00:05 +0100

    Add python-multipart and update startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/site/wwwroot
+export PORT=8000
+pip install --upgrade pip
 pip install -r requirements.txt
 gunicorn --bind=0.0.0.0:8000 --timeout 600 --worker-class uvicorn.workers.UvicornWorker main:app
